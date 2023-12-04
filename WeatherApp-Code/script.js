f48969d465532f11d571677d9978a437c5423552
Mazuros2 <s24440@pjwstk.edu.pl>
2023-12-04 09:59:19 +0100

    Html,css adjustments

---
+++ WeatherApp-Code/script.js
@@ -0,0 +1,25 @@
+//https://openweathermap.org/api
+
+function getWeather() 
+{
+
+
+}
+
+function getWeatherIcon() 
+{
+
+    
+}
+
+function getWeatherByHours() 
+{
+
+
+}
+
+function displayWeather()
+{
+
+
+}
