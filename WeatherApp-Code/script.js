30363fc3696b79127819927a420ba003de19f39d
Mazuros2 <s24440@pjwstk.edu.pl>
2023-12-05 17:11:37 +0100

    Function -> displayWeather() done.

--- WeatherApp-Code/script.js
+++ WeatherApp-Code/script.js
@@ -1,5 +1,4 @@
-//https://openweathermap.org/api
-const apiKey = "a8b3fe6c8983b2ed4650e647bfa2c257";
+const openWeatherMap_apiKey = "your api key from site"; //Here you should add your API Key from your profile on -> https://openweathermap.org/api
 
 const weather_temperature_Div = document.getElementById("weather-temperature");
 const weather_info_Div = document.getElementById("weather-info");
@@ -19,8 +18,29 @@ function displayWeather(weather_Data)
     weather_info_Div.innerHTML = "";
     weather_info_ByHour_Div.innerHTML = "";
 
+    if (weather_Data.cod === "404") 
+    {
+        weatherInfoDiv.innerHTML = `<p>${weather_Data.message}</p>`;
+        
+    } else {
+        const city_name = weather_Data.name; 
+        const temperature_in_City = Math.round(weather_Data.main.temp - 273.15);
+        const weather_description = weather_Data.weather[0].description;
+        
+        const icon_id = weather_Data.weather[0].icon;
+        const icon_url = `https://openweathermap.org/img/wn/${icon_id}@4x.png`;
 
-    getWeatherIcon();
+        const temperature_in_city_HTML = `<p>${temperature_in_City}°C</p>`;
+        const weather_description_Html = `<p>${city_name}</p> <p>${weather_description}</p>`;
+
+        weather_temperature_Div.innerHTML = temperature_in_city_HTML;
+        weather_info_Div.innerHTML = weather_description_Html;
+        
+        weather_Icon.src = icon_url;
+        weather_Icon.alt = weather_description;
+        getWeatherIcon();
+    }
+
 }
 
 function getWeatherByHours() 
