4573e6b77312ebf23fde6b8b72d1277cac83f467
Mazuros2 <s24440@pjwstk.edu.pl>
2023-12-05 12:03:13 +0100

    Begining of script.js; Small css update

--- WeatherApp-Code/script.js
+++ WeatherApp-Code/script.js
@@ -1,14 +1,21 @@
 //https://openweathermap.org/api
 
+const apiKey = "a8b3fe6c8983b2ed4650e647bfa2c257";
+
+const weather_temperature_Div = document.getElementById("weather-temperature");
+const weather_info_Div = document.getElementById("weather-info");
+const weather_info_ByHour_Div = document.getElementById("weather-info-ByHour");
+const weather_Icon = document.getElementById("weather-icon");
+
+
 function getWeather() 
 {
-
-
+  
 }
 
 function getWeatherIcon() 
 {
-
+    
     
 }
 
